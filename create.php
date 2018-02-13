5454774a2a1f9235e198b549406b094bce4dcbfa
Thomas <t.deruel@hotmail.fr>
2018-02-13 22:58:10 +0100

    update

--- create.php
+++ create.php
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/style.css">
-  <title>create</title>
-</head>
-  <body>
-    <div id="header" class="header">
-      <div class="container1">
-        <div class="flex">
-          <a class="headerLogo" href=""><img class="headerLogoimg" src="img/Logo.png" alt=""></a>
-
-          <ul class="mainNavigation">
-            <li class="listItem"><a class="listItem-link" href="/about.html">Actualités</a></li>
-            <li class="listItem"><a class="listItem-link" href="/blog.html">Innovations</a></li>
-            <li class="listItem"><a class="listItem-link" href="/podcast.html">Marques</a></li>
-            <li class="listItem"><a class="listItem-link" href="/reading.html">Recherche</a></li>
-            <a class="Search" href=""><img class="searchLogoimg" src="img/searchlogo.png" alt=""></a>
-          </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-
-    <section class="create">
-
-      <div class="create_left">
-      <p class="create_title">Modifier votre article</p>
-
-        <input class="input_container_title" type="text" placeholder="Titre de l'article">
-        <input class="input_container_place" type="text" placeholder="Lieux"><input class="input_container_date" type="date" placeholder="Titre de l'article"></div>
-
-      </div>
-      <textarea class="input_container_text"placeholder="Contenu de l'article"name="name" rows="8" cols="80"></textarea>
-      <div class="create_two">
-        <input class="input_container_imported"type="button" name="" value="Importer des images/vidéos ...">
-        <input class="input_container_saved"type="submit" name="" value="Enregistrer">
-      </div>
-    </section>
-  </body>
-</html>
-=======
 <!DOCTYPE html>
 <html>
 <head>
@@ -89,4 +42,3 @@
     </section>
   </body>
 </html>
->>>>>>> 7adac96450e0cfe39e14abc5c30b87484b259d09
--- modifier.php
+++ modifier.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>modifier</title>
-  </head>
-  <body>
-    <div id="header" class="header">
-      <div class="container1">
-        <div class="flex">
-          <a class="headerLogo" href=""><img class="headerLogoimg" src="img/Logo.png" alt=""></a>
-
-          <ul class="mainNavigation">
-            <li class="listItem"><a class="listItem-link" href="/about.html">Actualités</a></li>
-            <li class="listItem"><a class="listItem-link" href="/blog.html">Innovations</a></li>
-            <li class="listItem"><a class="listItem-link" href="/podcast.html">Marques</a></li>
-            <li class="listItem"><a class="listItem-link" href="/reading.html">Recherche</a></li>
-            <a class="Search" href=""><img class="searchLogoimg" src="img/searchlogo.png" alt=""></a>
-          </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <section class="create">
-      <div class="create_left">
-      <p class="create_title">Modifier votre article</p>
 <?php
 /**
  * Created by PhpStorm.
@@ -35,6 +5,7 @@
  * Date: 13/02/2018
  * Time: 00:55
  */
+
 require_once('database.php');
 
 if (isset($_GET['id'])) // On a le nom et le prénom
@@ -53,25 +24,6 @@ if (isset($_GET['id'])) // On a le nom et le prénom
         $description = $row['description'];
         $image = $row['image'];
 
-<<<<<<< HEAD
-        echo"<input class='input_container_title' type='text' placeholder='Titre de l'article' value=".$titre.">";
-
-        /*echo "<h1>Lieu</h1>";
-        echo "<input value=".$lieu."></input>";
-        echo "<br/>";*/
-
-        echo"<div class='floatRight'><input class='input_container_title' type='text' placeholder='Titre de l'article' value=".$date."></div>";
-
-
-        echo "<h1>Description</h1>";
-        echo "<input value=".$description."></input>";
-        echo "<br/>";
-
-        echo "<h1>URL de l'image</h1>";
-        echo "<input value=".$image."></input>";
-        echo "<br/>";
-=======
->>>>>>> 7adac96450e0cfe39e14abc5c30b87484b259d09
     }
 }
 else // Il manque des paramètres, on avertit le visiteur
@@ -79,12 +31,6 @@ else // Il manque des paramètres, on avertit le visiteur
     echo 'Il faut renseigner l\'id !';
 }
 ?>
-<<<<<<< HEAD
-</div>
-</section>
-</body>
-</html>
-=======
 
 <!DOCTYPE html>
 <html>
@@ -138,4 +84,3 @@ else // Il manque des paramètres, on avertit le visiteur
 </form>
 </body>
 </html>
->>>>>>> 7adac96450e0cfe39e14abc5c30b87484b259d09
